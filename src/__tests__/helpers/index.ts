20c972e595e659e0d1d50536cc4124fbaed94888
Adrien MILLE <adrien.mille.aer@gmail.com>
2022-03-10 15:13:34 +0800

    fix: solve missing export on unit testing helpers

--- src/__tests__/helpers/index.ts
+++ src/__tests__/helpers/index.ts
@@ -2,6 +2,7 @@ export * from './apollo';
 export * from './database';
 export * from './formData';
 export * from './storage';
+export { default as stopAllQueues } from '../../server/queues/stopAllQueues';
 
 export const composeHandlers =
     (...handlers: (() => Promise<void>)[]) =>
