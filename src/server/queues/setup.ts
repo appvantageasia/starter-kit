ddccdafef6b5eda05526dd008f07d86409a27b08
Adrien MILLE <adrien.mille.aer@gmail.com>
2022-04-08 22:08:14 +0800

    chore: improve setup of repeatable jobs

--- src/server/queues/setup.ts
+++ src/server/queues/setup.ts
@@ -1,22 +1,29 @@
-import { mainQueue } from './mainQueue';
+import config from '../core/config';
+import { QueuePeriodicPlans } from './QueueHandler';
+import { mainQueue, QueueMessage } from './mainQueue';
 import stopAllQueues from './stopAllQueues';
 
 export const queues = [mainQueue];
-
 const setup = (): (() => Promise<void>) => {
-    // setup periodic jobs on the main queue
-    mainQueue.setupWorker([
+    const mainQueueJobs: QueuePeriodicPlans<QueueMessage>[] = [
         // provide a periodic plan to send an email every hour
         {
             message: { value: 'periodic', type: 'dummy' },
             repeat: { cron: '0 * * * *' },
+            jobId: 'dummyRepeat',
         },
-        // run a worker beat every 10s
-        {
+    ];
+
+    if (config.healthChecks.workerBeat) {
+        mainQueueJobs.push({
             message: { type: 'workerBeat' },
             repeat: { every: 10000 },
-        },
-    ]);
+            jobId: 'workerBeat',
+        });
+    }
+
+    // setup periodic jobs on the main queue
+    mainQueue.setupWorker(mainQueueJobs);
 
     return async () => {
         // close queues
